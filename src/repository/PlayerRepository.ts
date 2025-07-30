6f4d6225faf9cc12c6dd84a095986d6d670b27e3
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-07-30 11:09:35 -0300

    implement json mock

--- src/repository/PlayerRepository.ts
+++ src/repository/PlayerRepository.ts
@@ -1,80 +1,13 @@
 import { Player } from "../model/Player";
+import fs from 'fs';
 
-const rawPlayers: Player[] = [
-  {
-    id: 1,
-    name: "Serhou Guirassy",
-    club: "Borussia Dortmund",
-    nationality: "Guiné-Bissau / França",
-    goals: 13,
-    minutes_played: 1084,
-    position: "Atacante"
-  },
-  {
-    id: 2,
-    name: "Raphinha",
-    club: "Barcelona",
-    nationality: "Brasil",
-    goals: 13,
-    minutes_played: 1225,
-    position: "Atacante"
-  },
-  {
-    id: 3,
-    name: "Robert Lewandowski",
-    club: "Barcelona",
-    nationality: "Polônia",
-    goals: 11,
-    minutes_played: 985,
-    position: "Atacante"
-  },
-  {
-    id: 4,
-    name: "Harry Kane",
-    club: "Bayern Munich",
-    nationality: "Inglaterra",
-    goals: 11,
-    minutes_played: 1120,
-    position: "Atacante"
-  },
-  {
-    id: 5,
-    name: "Lautaro Martínez",
-    club: "Inter Milan",
-    nationality: "Argentina",
-    goals: 9,
-    minutes_played: 857,
-    position: "Atacante"
-  },
-  {
-    id: 6,
-    name: "Ousmane Dembélé",
-    club: "Paris Saint‑Germain",
-    nationality: "França",
-    goals: 8,
-    minutes_played: 1163,
-    position: "Atacante",
-    awards: ["Player of the Season 2024‑25"]
-  },
-  {
-    id: 7,
-    name: "Désiré Doué",
-    club: "Paris Saint‑Germain",
-    nationality: "França",
-    position: "Meia / Atacante",
-    awards: ["Young Player of the Season 2024‑25"]
-  },
-  {
-    id: 8,
-    name: "Lamine Yamal",
-    club: "Barcelona",
-    nationality: "Espanha",
-    position: "Atacante / Meia"
-  }
-];
+const pathJson = "./src/data/players.json";
 
 class PlayerRepository {
   async getPlayers(): Promise<Player[]> {
+
+    const data = fs.readFileSync(pathJson, "utf-8")
+    const rawPlayers : Player[] = JSON.parse(data); 
     return rawPlayers.map(p => ({
       id: p.id,
       name: p.name,
@@ -85,18 +18,27 @@ class PlayerRepository {
   }
 
   async findPlayerById(id: number): Promise<Player | null> {
+    const data = fs.readFileSync(pathJson, "utf-8");
+    const rawPlayers : Player[] = JSON.parse(data);
     const player = rawPlayers.find(p => p.id === id);
     return player ?? null;
   }
 
   async createPlayer(player : Player) : Promise<Player | null>{
+    const data = fs.readFileSync(pathJson, "utf-8");
+    const rawPlayers : Player[] = JSON.parse(data);
     const search = rawPlayers.find(p => p.name === player.name || p.id === player.id);
     if(search) return null;
     rawPlayers.push(player);
+
+    fs.writeFileSync(pathJson, JSON.stringify(rawPlayers, null, 2), "utf-8");
+
     return player;
   }
 
   async updatePlayer(player : Player) : Promise<Player | null>{
+    const data = fs.readFileSync(pathJson, "utf-8");
+    const rawPlayers : Player[] = JSON.parse(data);
     const search = rawPlayers.find(p => p.id === player.id);
     if(!search) return null;
 
@@ -108,14 +50,21 @@ class PlayerRepository {
     search.position = player.position;
     search.awards = player.awards;
 
+    fs.writeFileSync(pathJson, JSON.stringify(rawPlayers, null, 2), "utf-8");
+
     return search;
   }
 
   async deletePlayerById(id : number) : Promise<Player | null>{
+    const data = fs.readFileSync(pathJson, "utf-8");
+    const rawPlayers : Player[] = JSON.parse(data);
     const index = rawPlayers.findIndex(p => p.id === id);
     if(index === -1) return null;
     const res = rawPlayers[index];
     rawPlayers.splice(index, 1);
+
+    fs.writeFileSync(pathJson, JSON.stringify(rawPlayers, null, 2), "utf-8");
+
 
     return res;
   }
